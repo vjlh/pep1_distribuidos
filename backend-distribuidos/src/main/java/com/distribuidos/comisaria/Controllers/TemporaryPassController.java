4c61f33d4e92d337d2fe8f0c82d049bcf3a5dd5f
albertorz31 <alberto.rodriguez.z@usach.cl>
2020-11-14 03:11:47 -0300

    arreglo chico

--- backend-distribuidos/src/main/java/com/distribuidos/comisaria/Controllers/TemporaryPassController.java
+++ backend-distribuidos/src/main/java/com/distribuidos/comisaria/Controllers/TemporaryPassController.java
@@ -4,9 +4,10 @@ import com.distribuidos.comisaria.Models.TemporaryPass;
 import com.distribuidos.comisaria.Repositories.TemporaryPassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.sql.Timestamp;
+
+import java.sql.Date;
 import java.util.ArrayList;
+
 
 
 @RestController
@@ -23,7 +24,7 @@ public class TemporaryPassController{
 
     @PostMapping("/newTemporarypass")
     public TemporaryPass createTemporaryPass(@RequestBody TemporaryPass newTemporaryPass){
-        newTemporaryPass.setRequestDate(new Timestamp(System.currentTimeMillis()));
+        newTemporaryPass.setRequestDate(new Date(System.currentTimeMillis()));
         newTemporaryPass.setLimitDate(newTemporaryPass.sumarHoras(newTemporaryPass.getRequestDate(), 2));
         return temporaryPassRepository.save(newTemporaryPass);
     }
