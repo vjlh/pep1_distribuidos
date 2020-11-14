e4238545694440506b07d83cbe652997153490c0
Vale <valentina.ligueno@gmail.com>
2020-11-13 21:33:36 -0300

    Inicio de Proyecto

---
+++ backend-distribuidos/src/main/java/com/distribuidos/comisaria/ComisariaApplication.java
@@ -0,0 +1,13 @@
+package com.distribuidos.comisaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComisariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComisariaApplication.class, args);
+	}
+
+}
---
+++ backend-distribuidos/src/test/java/com/distribuidos/comisaria/ComisariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribuidos.comisaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComisariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
